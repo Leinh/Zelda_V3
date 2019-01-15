10d778ac9d88d788e31563c43fa420c4d614b151
Leinh <antoine.tancrez@gmail.com>
2019-01-15 18:00:10 +0100

    test

--- script.js
+++ script.js
@@ -29,25 +29,27 @@ var css = 0;
 function night_mode() {
     if (css == 0) {
         document.getElementById("day_mode").href = "style_nuit.css"
-        setCookie("css", "nuit", 365);
+        setCookie("css", "nuit", 5);
         css = 1
     }
     else {
         document.getElementById("day_mode").href = "style.css"
-        setCookie("css", "jour", 365);
+        setCookie("css", "jour", 5);
         css = 0;
     }
-
+    if (getCookie("css") == "nuit") {
+        css = 1;
+        night_mode();
+    }
+    else if (getCookie("css") == "jour") {
+        css = 0;
+        night_mode();
+    }
 }
 
-if (getCookie("css") == "nuit") {
-    css = 1;
-    night_mode();
-}
-else if (getCookie("css") == "jour") {
-    css = 0;
-    night_mode();
-}
+night_mode();
+
+
 
 // Now the AJAX part. This one is particularly important since the website is imagined around this technology:
 
@@ -69,66 +71,66 @@ function loadDoc(x, y) {
 //TThe index
 function index() {
     loadDoc("ajax/index.txt", "ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Index";
+    document.getElementsByTagName("title")[0].innerHTML = "Index";
 }
 //First menu
 function histoire() {
     loadDoc("ajax/histoire.txt", "ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="L'histoire";
+    document.getElementsByTagName("title")[0].innerHTML = "L'histoire";
 }
 //Second menu
 function link() {
     loadDoc("ajax/link.txt", "ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Link";
+    document.getElementsByTagName("title")[0].innerHTML = "Link";
 }
 function zelda() {
     loadDoc("ajax/zelda.txt", "ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Zelda";
+    document.getElementsByTagName("title")[0].innerHTML = "Zelda";
 }
 function prodige() {
     loadDoc("ajax/prodige.txt", "ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les prodiges";
+    document.getElementsByTagName("title")[0].innerHTML = "Les prodiges";
 }
 function ganon() {
     loadDoc("ajax/ganon.txt", "ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Ganon";
+    document.getElementsByTagName("title")[0].innerHTML = "Ganon";
 }
 //Third menu
-function hylien(){
-    loadDoc("ajax/hylien.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les Hyliens";
+function hylien() {
+    loadDoc("ajax/hylien.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Les Hyliens";
 }
-function sheikah(){
-    loadDoc("ajax/sheikah.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les Sheikahs";
+function sheikah() {
+    loadDoc("ajax/sheikah.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Les Sheikahs";
 }
-function zora(){
-    loadDoc("ajax/zora.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les Zoras";
+function zora() {
+    loadDoc("ajax/zora.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Les Zoras";
 }
-function goron(){
-    loadDoc("ajax/goron.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les Gorons";
+function goron() {
+    loadDoc("ajax/goron.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Les Gorons";
 }
-function piaf(){
-    loadDoc("ajax/piaf.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les Piafs";
+function piaf() {
+    loadDoc("ajax/piaf.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Les Piafs";
 }
-function gerudo(){
-    loadDoc("ajax/gerudo.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les Gerudos";
+function gerudo() {
+    loadDoc("ajax/gerudo.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Les Gerudos";
 }
-function korogu(){
-    loadDoc("ajax/korogu.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Les Korogus";
+function korogu() {
+    loadDoc("ajax/korogu.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Les Korogus";
 }
 //The last menu
-function hyrule(){
-    loadDoc("ajax/hyrule.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="Hyrule";
+function hyrule() {
+    loadDoc("ajax/hyrule.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "Hyrule";
 }
 //The about menu
-function about(){
-    loadDoc("ajax/about.txt","ajax_content");
-    document.getElementsByTagName("title")[0].innerHTML="A propos";
+function about() {
+    loadDoc("ajax/about.txt", "ajax_content");
+    document.getElementsByTagName("title")[0].innerHTML = "A propos";
 }
